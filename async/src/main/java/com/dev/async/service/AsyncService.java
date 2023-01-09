f2a92b70d6f623b3195cbaf0df4678933d34e165
skskdmsdl <skskdmsdl@gmail.com>
2023-01-09 21:42:09 +0900

    async service에서 사용하는 email service 작성

--- async/src/main/java/com/dev/async/service/AsyncService.java
+++ async/src/main/java/com/dev/async/service/AsyncService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AsyncService {
 
+    private final EmailService emailService;
+
     public void asyncCall_1() {
         System.out.println("[asyncCall_1] :: " + Thread.currentThread().getName());
     }
---
+++ async/src/main/java/com/dev/async/service/EmailService.java
@@ -0,0 +1,20 @@
+package com.dev.async.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    @Async("defaultTaskExecutor")
+    public void sendMail() {
+        System.out.println("[sendMail] :: " + Thread.currentThread().getName());
+    }
+
+    @Async("messagingTaskExecutor")
+    public void messagingTaskExecutor() {
+        System.out.println("[messagingTaskExecutor] :: " + Thread.currentThread().getName());
+    }
+}
